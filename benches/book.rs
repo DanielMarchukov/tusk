c333c573ad5769b94204c03d32b7725b0ed2ab6d
DanielMarchukov <danielmarchukov@gmail.com>
2026-02-19 12:45:42 +0000

    Fix: clippy

--- benches/book.rs
+++ benches/book.rs
@@ -1,5 +1,6 @@
 use criterion::{criterion_group, criterion_main};
 
+#[allow(clippy::missing_const_for_fn)]
 fn book_benchmarks(_c: &mut criterion::Criterion) {}
 
 criterion_group!(benches, book_benchmarks);
--- benches/parser.rs
+++ benches/parser.rs
@@ -1,5 +1,6 @@
 use criterion::{criterion_group, criterion_main};
 
+#[allow(clippy::missing_const_for_fn)]
 fn parser_benchmarks(_c: &mut criterion::Criterion) {}
 
 criterion_group!(benches, parser_benchmarks);
